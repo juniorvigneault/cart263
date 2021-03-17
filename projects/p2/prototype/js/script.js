140383dc42c7dd05b2b01ee7878e9117f5bd9626
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2021-03-17 11:04:14 -0400

    P2: Trying to play with pixels inside the silhoutte of face

--- projects/p2/prototype/js/script.js
+++ projects/p2/prototype/js/script.js
@@ -21,35 +21,47 @@ function setup() {
   // Loading model and connect with video
   const facemesh = ml5.facemesh(video, modelReady);
 
+
   // store results in predictions (global variable)
   facemesh.on(`predict`, results => {
     predictions = results
 
-    // console.log(results)
+    console.log(results)
   });
 }
 
 function draw() {
   background(0);
-  imageMode(CENTER);
-  image(video, width/2, height/2, 1000, 750);
+  // image(video, width/2, height/2, 1000, 750);
+  image(video, 0,0);
+  keypoints();
 
-  keypoints()
 }
 
-// go through the array of points in and draw green ellipses on each 
+// go through the array of points in and draw green ellipses on each
 function keypoints() {
   for (let i = 0; i < predictions.length; i++) {
     const face = predictions[i];
-    for (let j = 0; j < face.scaledMesh.length; j ++) {
-      const keypoint = face.scaledMesh[j];
+    for (let j = 0; j < face.annotations.silhouette.length; j ++) {
+      const keypoint = face.annotations.silhouette[j];
       fill(0,255,0);
       noStroke();
-      ellipse(keypoint[0], keypoint[1], 5,5);
-
+      ellipse(keypoint[0], keypoint[1], 3,3);
     }
   }
 }
+
+// function keypoints() {
+//   for (let i = 0; i < predictions.length; i++) {
+//     const face = predictions[i];
+//     for (let j = 0; j < face.annotations.silhouette.length; j ++) {
+//       const keypoint = face.annotations.silhouette[j];
+//       fill(0,255,0);
+//       noStroke();
+//       ellipse(keypoint[0], keypoint[1], 3,3);
+//     }
+//   }
+// }
 
 function modelReady(){
   console.log(`MODEL IS READY`);
