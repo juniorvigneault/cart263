4b129806d467a8bc6c2b09cae6fab34199e851da
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2021-03-25 17:41:36 -0400

    P2: Started parent simulator
    • Added p5 and jquery ui elements

---
+++ projects/p2/prototype_3/js/Baby.js
@@ -0,0 +1,85 @@
+class Baby {
+  constructor(){
+    this.x = 400;
+    this.y = 300;
+    this.size = 100;
+    this.name = `BABY`;
+    this.color = {
+      r: 0,
+      g: 0,
+      b: 0
+    };
+    this.nameColor = 255;
+    this.nameSize = 32;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 0;
+    this.changeDirection = undefined;
+    this.angle = undefined;
+  }
+
+  update(){
+    this.displayBaby()
+    this.hunger();
+    this.move();
+    this.bored();
+    this.needLove();
+  }
+
+  needLove(){
+    this.size = constrain(this.size, 100, 200);
+    this.nameSize = constrain(this.nameSize, 32, 60);
+    this.size = this.size + 0.05;
+    this.nameSize = this.nameSize + 0.015;
+  }
+
+  hunger(){
+      let hungerSpeed = random(0,0.5);
+      this.color.g = constrain(this.color.g, 0, 255);
+      this.color.g = this.color.g + hungerSpeed;
+
+      $( "#progressbar" ).progressbar({
+        value: map(this.color.g, 0, 255, 0, 100),
+        complete: function(event,ui){
+          
+        }
+    });
+
+  };
+
+  bored(){
+    let boreSpeed = random(0,0.01);
+    this.speed = this.speed + boreSpeed;
+  }
+
+  move() {
+  this.changeDirection = random(0,2);
+  this.x = constrain(this.x, 4 + this.size/2, width - 4 - this.size/2);
+  this.y = constrain(this.y, 4 + this.size/2, height - 4 - this.size/2);
+
+  if (this.changeDirection < .01) {
+    this.vx = random(-this.speed, this.speed);
+    this.vy = random(-this.speed, this.speed);
+  }
+  this.x = this.x + this.vx;
+  this.y = this.y + this.vy;
+  }
+
+
+
+
+  displayBaby(){
+    push();
+    // baby body
+    ellipseMode(CENTER);
+    noStroke();
+    fill(this.color.r,this.color.g,this.color.b);
+    ellipse(this.x,this.y,this.size);
+    // baby name on body
+    textSize(this.nameSize);
+    fill(this.nameColor);
+    textAlign(CENTER,CENTER);
+    text(this.name, this.x, this.y);
+    pop();
+  }
+}
--- projects/p2/prototype_3/js/script.js
+++ projects/p2/prototype_3/js/script.js
@@ -1,21 +1,68 @@
 "use strict";
 
+let baby;
 
 function setup() {
-  createCanvas(1000, 1000)
+  // Create the canvas
+  let canvas = createCanvas(800, 600);
+  // Move the canvas within the HTML into the appropriate section
+  canvas.parent('p5js-canvas');
+  baby = new Baby();
+
+  $(`#feed`).on(`click`, function(event) {
+    console.log(`You fed the baby`);
+    feed(baby);
+  });
 }
 
 function draw() {
-  background(0);
+  background(200);
+  baby.update();
+};
+
+// to feed the baby, press f
+// to play with baby, press p
+// to change the baby, press c
+// to give love to baby, press l
+
+
+
+function keyPressed(){
+
+  if(key === `f`) {
+    feed(baby);
+    console.log(`You fed the baby`);
+  }
+  else if (key === `p`){
+    play(baby);
+    console.log(`You played with the baby`)
+  }
+  else if (key === `c`){
+    change(baby);
+    console.log(`You changed the diaper of the baby`)
+  }
+  else if (key === `l`){
+    love(baby);
+    console.log(`You gave love to the baby`)
+  }
 }
 
+// when feeding the baby, it returns to its original state (black)
+function feed(baby){
+  baby.color.g = baby.color.g - 200;
+}
 
+function play(baby){
+  baby.speed = 0
+}
 
+function change(baby){
+  baby.size = 100;
+  baby.nameSize = 32;
+}
 
-
-
-
-
+function love(baby){
+}
 
 // functions to translate RGB to HSLuv for fill and stroke
 function fillHsluv(h, s, l) {
@@ -26,9 +73,4 @@ function fillHsluv(h, s, l) {
 function strokeHsluv(h, s, l) {
   const rgb = hsluv.hsluvToRgb([h, s, l]);
   stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
-}
-
-// resizes the canvas
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
